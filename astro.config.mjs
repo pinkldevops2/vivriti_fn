f90408790a607416028833aec8202b84910fb805
Ajith Kumar A R <>
2026-01-16 16:00:13 +0530

    Config Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',             
-  adapter: vercel({}), 
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
