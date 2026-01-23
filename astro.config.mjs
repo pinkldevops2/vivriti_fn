8dd18502a8b353ebc5ccd61eaf9401071880cb2a
Ajith Kumar A R <>
2026-01-23 15:25:16 +0530

    News and Tech Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
